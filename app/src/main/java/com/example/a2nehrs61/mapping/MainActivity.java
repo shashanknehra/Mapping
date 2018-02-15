3dee44c088fd18466c4746c4464a35e9ad57c1b0
2nehrs61 <2nehrs61@solent.ac.uk>
2018-02-15 10:53:17 +0000

    prefActivity

--- app/src/main/java/com/example/a2nehrs61/mapping/MainActivity.java
+++ app/src/main/java/com/example/a2nehrs61/mapping/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.a2nehrs61.mapping;
 
 import android.app.AlertDialog;
+import android.content.SharedPreferences;
+import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -22,6 +24,7 @@ import org.osmdroid.views.MapView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     MapView mv;
+    double lastLat, lastLon;
 
 
     @Override
@@ -37,9 +40,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mv = (MapView)findViewById(R.id.map1);
         mv.setBuiltInZoomControls(true);
-        mv.getController().setZoom(2);
-        mv.getController().setCenter(new GeoPoint(50.9097,1.4044));
+    }
+    public void onStart()
+    {
+        super.onStart();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        double lat = Double.parseDouble ( prefs.getString("lat", "50.9") );
+        double lon = Double.parseDouble ( prefs.getString("lon", "-1.4") );
 
+        if (Math.abs(lat-lastLat)> 0.00001 || Math.abs(lon-lastLon) > 0.00001) {
+            mv.getController().setZoom(12);
+            mv.getController().setCenter(new GeoPoint(lat, lon));
+        }
+    }
+
+    public void onStop()
+    {
+        super.onStop();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        lastLat = Double.parseDouble ( prefs.getString("lat", "50.9") );
+        lastLon = Double.parseDouble ( prefs.getString("lon", "-1.4") );
+    }
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == R.id.choosemap)
+        {
+            Intent intent = new Intent(this,MapChooseActivity.class);
+            startActivityForResult(intent,0);
+            return true;
+        }
+        else if(item.getItemId() == R.id.preferences)
+        {
+            Intent intent = new Intent(this,PrefActivity.class);
+            startActivityForResult(intent,0);
+            return true;
+        }
+        return false;
     }
     public void onClick(View view)
     {
@@ -47,6 +89,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         EditText b = (EditText)findViewById(R.id.et2);
         double latitude = Double.parseDouble(a.getText().toString());
         double longitude = Double.parseDouble(b.getText().toString());
+
+
         if(latitude < -90.0 || latitude > 90.0){
             new AlertDialog.Builder(this).
                     setPositiveButton("OK",null).
@@ -63,5 +107,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
     }
+    protected void onActivityResult(int requestCode,int resultCode,Intent intent)
+    {
+
+        if(requestCode==0)
+        {
+
+            if (resultCode==RESULT_OK)
+            {
+                Bundle extras=intent.getExtras();
+                boolean hikebikemap = extras.getBoolean("com.example.hikebikemap");
+                if(hikebikemap==true)
+                {
+                    mv.setTileSource(TileSourceFactory.HIKEBIKEMAP);
+                }
+                else
+                {
+                    mv.setTileSource(TileSourceFactory.MAPNIK);
+                }
+            }
+        }
+    }
+
 
 }
